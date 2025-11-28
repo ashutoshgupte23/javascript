de62d723b53c4510d0f5afc0e65d355084bf07d6
ashutoshgupte23 <ashutoshofficial39@gmail.com>
2025-11-29 00:42:24 +0530

    functionsbasics

---
+++ 03_basics/01_function.js
@@ -0,0 +1,38 @@
+function sayMyname(){
+    // define the function(scope)
+    console.log("A")
+    console.log("s")         
+    console.log("h")
+    console.log("u")
+}
+//sayMyname=> reference 
+// () => execute
+// sayMyname()
+
+function addTwoNumber(num1,num2){
+    console.log(num1+num2) 
+}
+// addTwoNumber(2,2)
+
+
+function addTwoNumber(num1,num2){
+ 
+    // let result = num1 + num2
+    // return result
+    return num1 +num2
+}
+const result = addTwoNumber(2,2)
+//console.log("Result",result)
+
+
+/*
+
+| Value       | Behavior with `+`    |
+| ----------- | -------------------- |
+| `"2"`       | "22" (string concat) |
+| `null`      | 2 (null → 0)         |
+| `undefined` | NaN                  |
+| `true`      | 3 (true → 1)         |
+| `false`     | 2 (false → 0)        |
+
+*/
