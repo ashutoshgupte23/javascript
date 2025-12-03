c9c01813a5111df7f82a126c1b190cffec4ec614
ashutoshgupte23 <ashutoshofficial39@gmail.com>
2025-12-03 07:46:23 +0530

    while-loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+
+// while(...yaha pe hum condition check krte hai...) 
+
+let index = 0
+while (index < 10) {
+    // console.log(`value of the index ${index}`)
+index = index + 3
+}
+
+
+let arr =0
+let myArray = ['ashu','vishu','arjun','ritik']
+while(arr < myArray.length){
+
+    // console.log(`value is ${myArray[arr]}`)
+    arr ++
+}
+
+
+let score = 11
+do{
+    console.log(`value of score ${score}`)
+    score++
+
+}while(score<=10)
