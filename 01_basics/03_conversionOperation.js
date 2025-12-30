85261834cb2e46607bbf23f9bab25d7c5139be47
Ashutosh Gupte <ashutoshofficial39@gmail.com>
2025-12-30 23:14:23 +0530

    Create 03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score = "ashu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// ashu23 =>error(ashu not define show krega)
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+let isLoggedIn = "ashu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//   console.log(booleanIsLoggedIn);
+//   console.log(typeof booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+// null => false
+
+let someNumber = 35
+let randomString = String(someNumber)
+// console.log(typeof randomString)
+// undefined =>false
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+ 
+  console.log( typeof ("1" + 2));
+  console.log("1" + 2 + 2)
+  console.log( typeof(1 + 2 + "2"))
+  console.log(+true)
+  console.log(+"")
+  
+  let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
