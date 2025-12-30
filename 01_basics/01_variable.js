3c260f2db26ec49ca5af5d767f28a438c53568c5
Ashutosh Gupte <ashutoshofficial39@gmail.com>
2025-12-30 22:12:48 +0530

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,33 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+--------------------------------------------------------------------------
+
+//  var disadvantage
+// if(x==10){
+//     var x = 10
+// }
+//     console.log(x)
+
+// var block ke bahar bhi access ho jata hai, jo galat hai.
+
+// if (true) {
+//   let y = 10;
+  // console.log(y);
+// }
+
+// 👉 let sirf block ke andar kaam karta hai.
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
