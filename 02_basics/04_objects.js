9b1f1c6fc7c37afe1a747be8928584a5e7241bfd
Ashutosh Gupte <ashutoshofficial39@gmail.com>
2025-12-31 05:41:38 +0530

    04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+// const tinderUSer = new Object() =>singleton object
+// ✅ Singleton Object
+
+// 👉 Sirf ek hi instance (copy) banti hai poore program me.
+
+// 📌 JavaScript me jab object constructor se banate hain (new Object()), to wo singleton hota hai.
+
+// const user = new Object();
+// user.name = "Ashu";
+// user.age = 22;
+// console.log(user); 
+
+// Non-Singleton Object
+
+// 👉 Har baar naya object (new copy) banti hai.
+
+// 📌 JavaScript me jab object literal {} use karte hain, to non-singleton hota hai.
+
+// const user1 = {
+//   name: "Ashu"
+// };
+
+// const user2 = {
+//   name: "Ashu"
+// };
+
+// console.log(user1 === user2);
+
+
+
+
